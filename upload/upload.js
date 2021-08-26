dfadd2b39ef5dd49f33f29c4ef55a4a79c88bdc1
mosquito1717 <80042219+mosquito1717@users.noreply.github.com>
2021-08-26 09:45:46 +0900

    Add files via upload
    골격..

--- upload/upload.js
+++ upload/upload.js
@@ -1,31 +1,74 @@
-const upload = document.querySelector(".upload");
-const header = document.querySelector("header");
-const content = document.querySelector(".content");
-const afterUpload = document.querySelector(".afterUpload");
-const writing = document.querySelector("footer form .writing");
-const search = document.querySelector("footer form .search");
-const mypage = document.querySelector("footer form .mypage");
+import React, {Component} from "react";
+import { StyleSheet, Text, View, Buttonm, TextInput } from "react-native";
+import { Image } from "react-native";
 
-function handleUploadClick() {
-  header.classList.add("hidden");
-  content.classList.add("hidden");
-  afterUpload.classList.remove("hidden");
-  afterUpload.innerText = "업로드가 완료됐습니다.";
+export default class myPage extends Component() {
+  return (
+      // 개인정보 수정창으로 이동
+    movetoPersonalInfo(){
+
+    }
+    // 팔로우창으로 이동
+    movetofollow(){
+
+    }
+    // 팔로워창으로 이동
+    movetofollower(){
+
+    }
+    // 글 갤러리창으로 이동
+    movetoWriting(){
+
+    }
+    //검색창으로 이동
+    movetoSearch(){
+
+    }
+    // 내 페이지로 이동
+    movetoMyPage(){
+
+    }
+
+    <View style={styles.container}>
+      <View style={styles.title}> title </View>
+      <View style={styles.content}>
+          <View style={styles.image}><Text style={styles.imageText}>사진: </Text></View>
+          <View style={styles.text}><Text style={styles.textText}>글: </Text><TextInput value={} style={styles.textInput}></TextInput></View>
+      </View>
+      <View style={styles.navigator}>
+          <Button style={styles.writing} title="글" onPress={this.movetoWriting}/>
+          <Button style={styles.search} title="검색" onPress={this.movetoSearch}/>
+          <Button style={styles.myPage} title="내 페이지" onPress={this.movetoMypage}/>
+      </View>
+    </View>
+  );
 }
 
-function handleWritingClick() {
-  location.href = ""; // 글 창으로 이동
-}
-
-function handleSearchClick() {
-  location.href = ""; // 글 창으로 이동
-}
-
-function handleMypageClick() {
-  location.href = ""; // 글 창으로 이동
-}
-
-upload.addEventListener("click", handleUploadClick);
-writing.addEventListener("click", handleWritingClick);
-search.addEventListener("click", handleSearchClick);
-mypage.addEventListener("click", handleMypageClick);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {},
+  content: {},
+  image: {},
+  imgText: {
+    color: "white",
+  },
+  text: {},
+  textText: {},
+  textInput: {},
+  profile: {},
+  profileImg: {},
+  nameNmodify: {},
+  name: {},
+  button: {},
+  friends: {},
+  contentImg: {},
+  navigator: {},
+  writing: {},
+  search: {},
+  myPage: {},
+});
