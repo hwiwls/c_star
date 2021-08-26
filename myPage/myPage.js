095ff9f4a59de953de2490f84ad4ef24ab587572
mosquito1717 <80042219+mosquito1717@users.noreply.github.com>
2021-08-26 09:45:25 +0900

    Add files via upload
    골격

--- myPage/myPage.js
+++ myPage/myPage.js
@@ -1,37 +1,89 @@
-const ModifyPersonalInfo = document.querySelector("#modifyPersonalInfo");
-const follow = document.querySelector(".friends .follow");
-const follower = document.querySelector(".friends .follower");
-const writing = document.querySelector("footer form .writing");
-const search = document.querySelector("footer form .search");
-const mypage = document.querySelector("footer form .mypage");
+import React, {Component} from "react";
+import { StyleSheet, Text, View, Botton } from "react-native";
+import { Image } from "react-native";
 
-function handleMPIClick() {
-  location.href = "https://www.naver.com"; // 개인정보 수정창으로 이동
+export default class myPage extends Component() {
+  return (
+      // 개인정보 수정창으로 이동
+    movetoPersonalInfo(){
+
+    }
+    // 팔로우창으로 이동
+    movetofollow(){
+
+    }
+    // 팔로워창으로 이동
+    movetofollower(){
+
+    }
+    // 글 갤러리창으로 이동
+    movetoWriting(){
+
+    }
+    //검색창으로 이동
+    movetoSearch(){
+
+    }
+    // 내 페이지로 이동
+    movetoMyPage(){
+
+    }
+
+    <View style={styles.container}>
+      <View style={styles.header}>
+            <View style={styles.profile}>
+                <Image style={styles.profileImg} source={require("")}></Image>
+                <View style={styles.nameNmodify}>
+                    <Text style={styles.name}>이름</Text>
+                    <Button style={styles.button} title="개인정보 수정" onPress={this.movetoPersonalInfo} />
+                </View>
+        </View>
+        <View style={styles.friends}>
+            <Button style={styles.follow} title="팔로우" onPress={this.movetofollow}/>
+            <Button style={styles.follower} title="팔로워" onPress={this.movetofollower}/>
+        </View>
+      </View>
+      <View style={styles.content}>
+          <Image style={styles.contentImg}></Image>
+          <Image style={styles.contentImg}></Image>
+          <Image style={styles.contentImg}></Image>
+          <Image style={styles.contentImg}></Image>
+          <Image style={styles.contentImg}></Image>
+          <Image style={styles.contentImg}></Image>
+          <Image style={styles.contentImg}></Image>
+          <Image style={styles.contentImg}></Image>
+          <Image style={styles.contentImg}></Image>
+      </View>
+      <View style={styles.navigator}>
+          <Button style={styles.writing} title="글" onPress={this.movetoWriting}/>
+          <Button style={styles.search} title="검색" onPress={this.movetoSearch}/>
+          <Button style={styles.myPage} title="내 페이지" onPress={this.movetoMypage}/>
+      </View>
+    </View>
+  );
 }
 
-function handleFollowClick() {
-  location.href = "https://www.naver.com"; // 팔로우 창으로 이동
-}
-
-function handleFollowerClick() {
-  location.href = "https://www.naver.com"; // 팔로워 창으로 이동
-}
-
-function handleWritingClick() {
-  location.href = ""; // 글 창으로 이동
-}
-
-function handleSearchClick() {
-  location.href = ""; // 글 창으로 이동
-}
-
-function handleMypageClick() {
-  location.href = ""; // 글 창으로 이동
-}
-
-ModifyPersonalInfo.addEventListener("click", handleMPIClick);
-follow.addEventListener("click", handleFollowClick);
-follower.addEventListener("click", handleFollowerClick);
-writing.addEventListener("click", handleWritingClick);
-search.addEventListener("click", handleSearchClick);
-mypage.addEventListener("click", handleMypageClick);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {},
+  text: {
+    color: "white",
+  },
+  profile: {},
+  profileImg: {},
+  nameNmodify: {},
+  name: {},
+  button: {},
+  friends: {},
+  content: {},
+  contentImg: {},
+  navigator: {},
+  writing: {},
+  search: {},
+  myPage: {},
+});
