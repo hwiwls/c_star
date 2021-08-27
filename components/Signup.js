cbf10c2d44fab2d00a9120ee810a28472a6473cb
kang hyeon <hyeonkang.dev@gmail.com>
2021-08-27 16:16:22 +0900

    can send signup info to database

--- components/Signup.js
+++ components/Signup.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import firebase from "firebase";
+import database from "@react-native-firebase/database";
 
 export default class SignupScreen extends React.Component {
   constructor(props) {
@@ -13,12 +14,52 @@ export default class SignupScreen extends React.Component {
     };
   }
 
+  componentDidMount() {
+    // 파이어베이스 연결 ok
+    const firebaseConfig = {
+      apiKey: "AIzaSyAdzYxjy0F4G9bZ7qiHLvmJQy31c76PAFc",
+      authDomain: "cstar-a5fed.firebaseapp.com",
+      databaseURL:
+        "https://cstar-a5fed-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "cstar-a5fed",
+      storageBucket: "cstar-a5fed.appspot.com",
+      messagingSenderId: "486423499209",
+      appId: "1:486423499209:web:67c80e204a084978abd818",
+      measurementId: "G-SC0J1J3CHT",
+    };
+
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    } else {
+      firebase.app(); // if already initialized, use that one
+    }
+  }
+
   // 데이터를 어떻게 보내지
   checkSignup = () => {
     console.log("checkSignup");
     console.log(this.state.name);
     console.log(this.state.id);
     console.log(this.state.pwd);
+
+    // 모두 제대로 입력되었는지 확인
+
+    // member data 추가
+    this.addMemberData(this.state.id, this.state.name, this.state.pwd);
+  };
+
+  // member data에 새로운 member을 추가하는 함수
+  addMemberData = (userid, username, userpwd) => {
+    firebase
+      .database()
+      .ref("member/" + userid)
+      .set({
+        name: username,
+        id: userid,
+        pwd: userpwd,
+      })
+      .then(console.log("complete"));
   };
 
   render() {
